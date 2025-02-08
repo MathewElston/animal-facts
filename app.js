aa7ff27cf140ed01f2a51ca01344e18e7e05c6ed
Mathew Elston <elstonmathew@yahoo.com>
2025-02-07 20:57:10 -0800

    Implmented fetch API Function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Animal API call function and headers -> Returns Async JSON Object
 const animalHeaders = new Headers({
   "X-Api-Key": "VmBqdp0DySLZmHEVc+jqqQ==Nf2BTvpDoqncCtKW",
 });
@@ -13,6 +14,31 @@ async function fetchAnimals(args) {
 
     console.log(data);
     console.log(response);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Images API call function and headers -> Returns Async JSON Object
+const imagesHeaders = new Headers({
+  Authorization: "Qna0ffg6Y6hV8qsX7hBYb92BxvTFW6fATIb0eMJ9TTQzlCnpp9GG9EmK",
+});
+const imagesURL = "https://api.pexels.com/v1/search?";
+
+async function fetchImages(args) {
+  try {
+    const response = await fetch(imagesURL + args, { headers: imagesHeaders });
+    if (!response.ok) {
+      throw new Error("Could not fetch API.");
+    }
+    const data = await response.json();
+
+    console.log(data);
+    console.log(response);
+
+    return data;
   } catch (error) {
     console.log(error);
   }
