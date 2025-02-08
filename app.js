0dd22bd42f68af2794a29242ee1cc212870d6e28
Mathew Elston <elstonmathew@yahoo.com>
2025-02-07 20:12:07 -0800

    implmented animals API with Header

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-testAPI = "https://jsonplaceholder.typicode.com/todos/";
+const animalHeaders = new Headers({
+  "X-Api-Key": "VmBqdp0DySLZmHEVc+jqqQ==Nf2BTvpDoqncCtKW",
+});
+const animalsURL = "https://api.api-ninjas.com/v1/animals?";
+
+async function fetchAnimals(args) {
+  try {
+    const response = await fetch(animalsURL + args, { headers: animalHeaders });
+    if (!response.ok) {
+      throw new Error("Could not fetch API.");
+    }
+    const data = await response.json();
+
+    console.log(data);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchAnimals("name=cheetah");
