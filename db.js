d33216a1d50fcb901f77bc2f091e6d844b10454d
VictorG411 <vguzman2@students.columbiabasin.edu>
2025-02-11 13:02:57 -0800

    added function defintions for managing history and favorites

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 // this object effectively acts as a namespace for our database methods and properties
 var db = {
+    localStorageAccess : false, // whether or not we can access localStorage
 
     // checks whether localStorage is available for use
+    // returnes true if avaiable, and false if not
+    // also sets the localStorageAccess flag
     checkStorage : function() {
         try {
             localStorage.setItem("TestKey", "TestValue");
             localStorage.removeItem("TestKey");
+            localStorageAccess = true;
             return true;
         }
         catch (e) {
+            localStorageAccess = false;
             return (
             e instanceof DOMException &&
             e.name === "QuotaExceededError" &&
@@ -20,8 +25,43 @@ var db = {
         
     },
 
-    // add a new key value pair to the database
-    new : function(key, value) {
+    // add a new favorite animal, will automatically take a timestamp as well
+    // returns true if successful and false if not
+    addFavorite : function(name, desc, image) {
+        if (!localStorageAccess) { return false; }
 
+    },
+
+    // remove a favorite animal by name
+    // returns true if favorite was removed and false if no such favorite
+    // remove functions ignore localStorageAccess and attempt to delete anyway since storage may just be full
+    // they also run checkStorage again to see if there's now free space to store new information
+    removeFavorite : function(name) {
+
+        this.checkStorage();
+        return true;
+    },
+
+    // add to user history, will automatically take a timestamp as well
+    // returns true if successful and false if not
+    addHistory : function(name, desc, image) {
+        if (!localStorageAccess) { return false; }
+
+    },
+
+    // remove from user history by name
+    // returns true if history was removed and false if no such history
+    removeHistory : function (name) {
+
+        this.checkStorage();
+        return true;
+    },
+
+    // clear all user history
+    clearHistory : function () {
+        if (!localStorageAccess) { return false; }
+
+        this.checkStorage();
+        return true;
     }
 }
