2854a0e29e63388ff2ce4c2398046502030b18aa
Mathew Elston <elstonmathew@yahoo.com>
2025-02-11 20:28:55 -0800

    added bootstrap column  and corrected nesting

--- createFactCard.js
+++ createFactCard.js
@@ -3,14 +3,14 @@ const placeholderData = {
 };
 async function createFactCard(parentId, title, text) {
   const parent = document.getElementById(parentId);
+  const colDiv = document.createElement("div");
   const cardDiv = document.createElement("div");
   const cardBody = document.createElement("div");
   const cardTitle = document.createElement("h5");
   const cardText = document.createElement("p");
 
-  const children = [cardDiv, cardBody, cardTitle, cardText];
-
-  cardDiv.classList.add("card");
+  colDiv.classList.add("col-md-4");
+  cardDiv.classList.add("card", "mb-3");
   cardBody.classList.add("card-body");
   cardTitle.classList.add("card-title");
   cardText.classList.add("card-text");
@@ -18,9 +18,11 @@ async function createFactCard(parentId, title, text) {
   cardTitle.innerText = title;
   cardText.innerText = text;
 
-  for (const child of children) {
-    parent.appendChild(child);
-  }
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardDiv.appendChild(cardBody);
+  colDiv.appendChild(cardDiv);
+  parent.appendChild(colDiv);
 }
 
 function testCard() {
