b00264a92c75a92e0354f86d1ed24b86835ed6aa
monique <msantoyo@columbiabasin.edu>
2025-02-12 20:33:43 -0800

    Update search animal function appendChild Bug

--- searchbar.js
+++ searchbar.js
@@ -2,9 +2,15 @@ async function searchAnimal() {
     //User's input
     const searchInput = document.getElementById('searchInput').value;
     // Search results are displayed
-    const resultDiv = document.getElementById('result');
+    const resultDiv = document.getElementById('results');
+
+    const resultHeader = document.createElement("h3");
     //Fetches animals 
     const animals = await fetchAnimals("name="+searchInput);
+    const firstResult = animals[0].name;
+    
+    resultHeader.innerText = firstResult;
+    resultDiv.appendChild(resultHeader);
     console.log(animals[0].name);
     // Then works on going through the animal 
     // resultDiv.innerText = '';
