05e90b3b67d980a2e91364fb9d41bba1aa5f6a3a
monique <msantoyo@columbiabasin.edu>
2025-02-12 20:15:07 -0800

    Updated js code for searchbar

--- searchbar.js
+++ searchbar.js
@@ -4,15 +4,16 @@ async function searchAnimal() {
     // Search results are displayed
     const resultDiv = document.getElementById('result');
     //Fetches animals 
-    const animals = await fetchAnimals(searchInput);
-    
+    const animals = await fetchAnimals("name="+searchInput);
+    console.log(animals[0].name);
     // Then works on going through the animal 
-    resultDiv.innerHTML = '';
-    animals.forEach(animal => {
-        const animalDiv = document.createElement('div');
-        animalDiv.innerHTML = `
-            <h3>${animal.name}</h3>
-        `;
-        resultDiv.appendChild(animalDiv);
-    });
+    // resultDiv.innerText = '';
+    // animals.forEach(animal => {
+    //     const animalDiv = document.createElement('div');
+    //     animalDiv.innerText = `
+    //         <h3>${animal.name}</h3>
+    //     `;
+    //     resultDiv.appendChild(animalDiv);
+    //     console.log(animal.name);
+    // });
 }
