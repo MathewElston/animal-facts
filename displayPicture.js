d75a543a3f76ced7f7f6ca3aa156a8a497771a31
Your Name <you@example.com>
2025-02-20 19:22:41 -0800

    referencing

--- displayPicture.js
+++ displayPicture.js
@@ -2,12 +2,20 @@ testData = "Cheetah";
 
 async function displayPicture(animal) {
     const data = await fetchImages("query="+animal); 
-    const imageSource = data.photos[0].src.original
+    const imageSource = data.photos[0].src.original// Gets the first index
     const img = document.createElement("img");
-    const photoParent = document.getElementById('photo-container');
+    const photoParent = document.getElementById('photo-container').addEventListener("click", placeholder);//grabs the photo comntainer element
+       
     img.src = imageSource;
-    img.width = 500;
-    img.height = 350;
-    photoParent.appendChild(img);
+    img.width = 500;//adjusts the width of the image
+    img.height = 350;//adjusts the height of the image
+    photoParent.appendChild(img);//
     console.log(data.photos[0]);
 }
+
+async function getImage()
+{
+    const data = await fetchImages("query="+animal);
+    retreivedata;
+}
+    
