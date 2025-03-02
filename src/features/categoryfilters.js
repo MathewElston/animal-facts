71e2ccd24d42f0a0dd8a78dbc42f8edeab8d3a4a
Your Name <you@example.com>
2025-03-01 16:48:51 -0800

    stuff

--- src/features/categoryfilters.js
+++ src/features/categoryfilters.js
@@ -1,15 +1,27 @@
 async function dropDownMenu()
 {
-    //const crtAt = document.createAttribute("class");
-    //crtAt.value ="dropdown";
-    //const buttonFun = document.createElement("button");
-document.getElementById("sections").classList.toggle("show");
-
-document.getElementById("a","#Wetland").innerHTML(fetchAnimals());
-document.getElementById("a","#Grassland").innerHTML(fetchAnimals());
-document.getElementById("a","#openocean").innerHTML(fetchAnimals());
-document.getElementById("a","#Tundra").innerHTML(fetchAnimals());
-document.getElementById("a","#Savanna").innerHTML(fetchAnimals());
+ document.getElementById("sections").classList.toggle("show").styles.css;
+ document.getElementsByClassName("content");
+ document.getElementsByClassName("content").onclick = fetchAnimals().utils(fetch.js);//search input maybe
+/*
+if(filter.display === "block")
+{
+    filter.display = "none";
 }
+else
+{
+    filter.display = "block";
+}
+*/
+//content.innerHTML = fetchAnimals.utils(fetch.js);
+//contentClick.onclick.innerHTML = fetchAnimals.utils(fetch.js);
+
+
+//document.getElementsByClassName("content","#Grassland").innerHTML=fetchAnimals().utils;
+//document.getElementsByClassName("content","#openocean").innerHTML=fetchAnimals().utils;
+//document.getElementsByClassName("content","#Tundra").innerHTML=fetchAnimals().utils;
+//document.getElementsByClassName("content","#Savanna").innerHTML=fetchAnimals().utils;
+}//need to either write CSS to be able to toggle the dropdownbutton or find something else?
+
 
 
